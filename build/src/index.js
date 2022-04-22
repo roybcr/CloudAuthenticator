83300cf8d5c27137dc2ef9c1d3bd7f268f9fc1b3
Roy <roybcx@pm.me>
2022-04-22 21:05:18 +0300

    -

--- build/src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.cloudAuthenticator = void 0;
-const cloudAuthenticator = (req, res) => {
-    res.send('Hello, World');
-};
-exports.cloudAuthenticator = cloudAuthenticator;
-//# sourceMappingURL=index.js.map
